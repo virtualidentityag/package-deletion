828ec6a80f8a78ea111bcda7f656e0e055824b08
Lars Brümmer <lars.bruemmer@virtual-identity.com>
2021-09-06 13:10:27 +0200

    update-for-registry
    - added tests
    - fixed code

--- select.js
+++ select.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core');
+const {filterVersions, deleteVersions} = require('functions');
 require('isomorphic-fetch');
 
 const main = async () => {
@@ -21,7 +22,7 @@ const main = async () => {
           console.log("[" + res.status + "] Successfully loaded packages");
           return res.json();
         } else {
-          throw "[" + res.status + "] Something went wrong";
+          throw new Error("[" + res.status + "] Something went wrong");
         }
       })
       .then(resJson => filterVersions(resJson, numberOfRcToKeep, numberOfSnapshotsToKeep, numberOfFeatureSnapshotsToKeep))
@@ -31,74 +32,6 @@ const main = async () => {
         console.error(error);
         core.setFailed(error.message);
       });
-}
-
-function filterVersions(json, numberOfRcToKeep, numberOfSnapshotsToKeep, numberOfFeatureSnapshotsToKeep) {
-  if (json === undefined) {
-    return undefined;
-  }
-
-  let mappedData = json.filter(e => {
-    return e.metadata.container.tags.length > 0;
-  }).map(e => {
-    let data = {};
-    data.id = e.id;
-    data.version = e.metadata.container.tags[0];
-    data.updated_at = e.updated_at;
-    return data;
-  });
-
-  // Filter out release candidates
-  let releaseCandidatesToDelete = filterSortAndSlice(mappedData, /^[0-9]+\.[0-9]+\.[0-9]+-rc.*$/gi, numberOfRcToKeep);
-
-  // Filter out snapshots
-  let snapshotsToDelete = filterSortAndSlice(mappedData, /^[0-9]+\.[0-9]+\.[0-9]+-snapshot$/gi, numberOfSnapshotsToKeep);
-
-  // Filter out feature branches
-  let featureBranchesToDelete = filterSortAndSlice(mappedData, /^[0-9]+\.[0-9]+\.[0-9]+-snapshot-.*$/gi, numberOfFeatureSnapshotsToKeep);
-
-  return releaseCandidatesToDelete.concat(snapshotsToDelete, featureBranchesToDelete);
-}
-
-function filterSortAndSlice(mappedData, regex, numberToKeep) {
-  return mappedData
-      .filter(e => regex.test(e.version))
-      .sort((a, b) => sortByDateDescending(a, b))
-      .slice(numberToKeep, this.length);
-}
-
-function sortByDateDescending(a, b) {
-  return new Date(b.updated_at) - new Date(a.updated_at);
-}
-
-function deleteVersions(versions) {
-  if (versions === undefined || versions.length === 0) {
-    console.log("Nothing to delete");
-
-    return "";
-  }
-
-  versions.forEach(version => deleteVersion(version));
-
-  return versions.map(version => version.version).join();
-}
-
-function deleteVersion(version, owner, packageName, token) {
-  console.log("Deleting version " + version.version);
-
-  fetch('https://api.github.com/orgs/' + owner + '/packages/container/' + encodeURIComponent(packageName) + '/versions/' + version.id + '?package_type=container&visibility=internal', {
-    method: 'DELETE',
-    headers: {
-      'Accept': 'application/vnd.github.v3+json',
-      'authorization': `token ${token}`,
-    }
-  }).then(res => {
-    if (res.status === 204) {
-      console.log("[" + res.status + "] Successfully deleted version " + version.version);
-    } else {
-      console.error("[" + res.status + "] Something went wrong for version " + version.version)
-    }
-  });
 }
 
 main().catch(error => {
